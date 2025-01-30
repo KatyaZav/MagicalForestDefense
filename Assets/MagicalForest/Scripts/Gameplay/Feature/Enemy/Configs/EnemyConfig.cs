2083856d4f3959b05b3ba2194377ab6f05e45434
Baronne_Noire <party2015ru@mail.ru>
2025-01-30 12:25:26 +0500

    change configs

--- Assets/MagicalForest/Scripts/Gameplay/Feature/Enemy/Configs/EnemyConfig.cs
+++ Assets/MagicalForest/Scripts/Gameplay/Feature/Enemy/Configs/EnemyConfig.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using Assets.Servises.Configs;
+using UnityEngine;
 
 namespace Assets.Gameplay.Features.Enemy
 {
     [CreateAssetMenu(fileName = "EnemyData", menuName = "Configs/Gameplay/Enemy/EnemyData", order = 1)]
     public class EnemyConfig : ScriptableObject
     {
-        [field:SerializeField] public string Name {  get; private set; }
+        [field:SerializeField] public TextTranslateConfig Name {  get; private set; }
+        [field: SerializeField] public int Reward { get; private set; } = 10;
         [field:SerializeField] public int Speed { get; private set; }
         [field:SerializeField] public int Health { get; private set; }
         [field:SerializeField] public GameObject Model { get; private set; }
