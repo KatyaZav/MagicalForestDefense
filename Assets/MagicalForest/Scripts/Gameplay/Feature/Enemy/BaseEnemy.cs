55dc528d8220d263a754887f1dcdd05bcc503c50
Baronne_Noire <party2015ru@mail.ru>
2025-02-05 12:54:21 +0500

    add enemy spawner

--- Assets/MagicalForest/Scripts/Gameplay/Feature/Enemy/BaseEnemy.cs
+++ Assets/MagicalForest/Scripts/Gameplay/Feature/Enemy/BaseEnemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,9 @@ namespace Assets.Gameplay.Features.Enemy
 {
     public class BaseEnemy : MonoBehaviour
     {
-        // Start is called before the first frame update
+        public event Action<BaseEnemy> Died;
+
+               // Start is called before the first frame update
         void Start()
         {
 
